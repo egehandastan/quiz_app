d481a353444d714b05a310c91f1b09edc5cb2493
Egehan Daştan <87079212+egehandastan@users.noreply.github.com>
2023-09-13 00:00:14 +0300

    button navigation

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradient_container.dart';
+
 
 class StartScreen extends StatelessWidget{
-const StartScreen({super.key});
-   @override
- Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          colors: [
-            Color.fromARGB(255, 47, 4, 120),
-            Color.fromARGB(255, 83, 46, 146),
-            Color.fromARGB(255, 94, 65, 144),
+const StartScreen(this.startQuiz,{super.key});
+  
+  final void Function() startQuiz;
+ 
+   
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              "assets/images/quiz-logo.png",
+              width: MediaQuery.of(context).size.width * 0.65,
+              color: const Color.fromARGB(125, 255, 255, 255),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            const Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                  fontSize: 24, color: Color.fromARGB(255, 230, 188, 254)),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            OutlinedButton.icon(
+                onPressed: () {
+                  startQuiz();
+                },
+                style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+                icon: const Icon(Icons.arrow_right_alt),
+                label: const Text('Start Quiz'),
+                ),
           ],
-          
         ),
-        
-      ),
+      
     );
   }
+  
 }
